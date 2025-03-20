9d02df0602b0b834ed0fc14847edbd6c38109b75
Sreeja5104 <gannamanisreeja@gmail.com>
2025-03-20 14:47:25 +0530

    Update Profile.jsx

--- src/components/Layout/Profile.jsx
+++ src/components/Layout/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/profile.css";
+import "../Styles/profile.css";
 
 const Profile = () => {
   const [user, setUser] = useState({
