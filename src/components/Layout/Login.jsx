126a6ab424766f77a2e5ce0d06377d47e893fa17
Sreeja5104 <gannamanisreeja@gmail.com>
2025-03-20 14:47:42 +0530

    Update Login.jsx

--- src/components/Layout/Login.jsx
+++ src/components/Layout/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/login.css";
+import "../Styles/login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
